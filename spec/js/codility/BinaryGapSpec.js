0af18299408386a792c90a6a3539687208681042
Andy McCall <mailme@andymccall.co.uk>
2018-03-15 16:05:46 +0000

    Added jasmine standalone and fixed unit testing

---
+++ spec/js/codility/BinaryGapSpec.js
@@ -0,0 +1,11 @@
+describe('BinaryGap', function () {
+    it('Test against number 1024, expect 5', function () {
+        expect(solution(1041)).toEqual(5);
+    });
+});
+
+describe('BinaryGap', function () {
+    it('Test against number 9, expect 2', function () {
+        expect(solution(9)).toEqual(2);
+    });
+});
