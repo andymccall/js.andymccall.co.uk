38ae27c377f7bd6d2a9c38b3cd7a9bfe5f3daefd
Andy McCall <mailme@andymccall.co.uk>
2018-03-19 16:23:45 +0000

    Added AJAX example

---
+++ src/js/ajaxexample.js
@@ -0,0 +1,11 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "../resources/ajaxexample.txt", true);
+    xhttp.send();
+}
