6e510d83aae346ee7d61e44753f95cc2b8e6df33
Andy McCall <mailme@andymccall.co.uk>
2018-03-26 23:03:25 +0100

    Added dynamic sizing of LCD digits

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,11 +1,12 @@
-var tempNumber=0;
-var clearOnNextInput=true;
+var tempNumber = 0;
+var clearOnNextInput = true;
+var quantityOfNumbers = 0;
 
 function initCalculator() {
 
 }
 
-function getExistingDigits(){
+function getExistingDigits() {
     var p = document.getElementById('digits');
     var text = p.textContent;
     return Number(text);
@@ -13,13 +14,25 @@ function getExistingDigits(){
 
 function numberButton(buttonPressed) {
     var number = getExistingDigits();
+    quantityOfNumbers++;
 
-    if (clearOnNextInput) {
+    if (quantityOfNumbers > 7) {
+        document.getElementById("digits").style.fontSize = "22px";
+    }
+    if (quantityOfNumbers > 15) {
+        document.getElementById("digits").style.fontSize = "16px";
+    }
+    if (quantityOfNumbers > 17) {
+        document.getElementById("digits").style.fontSize = "48px";
+        document.getElementById("digits").innerText = "ERROR";
+        clearOnNextInput = true;
+        quantityOfNumbers = 0;
+    } else if (clearOnNextInput) {
         document.getElementById("digits").innerText = buttonPressed;
+        clearOnNextInput = false;
     } else {
         document.getElementById("digits").innerText = number + buttonPressed.toString();
     }
-    clearOnNextInput = false;
 }
 
 function plusButton() {
@@ -37,6 +50,8 @@ function equalsButton() {
     clearOnNextInput = true;
 }
 
-function allClearButton() {
-    document.getElementById("digits").innerText="0";
+function oncButton() {
+    quantityOfNumbers = 0;
+    document.getElementById("digits").innerText = "0";
+    document.getElementById("digits").style.fontSize = "48px";
 }
